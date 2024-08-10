550115e4b5aa335dde0b401b8ef7fd4efd83b493
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2024-08-10 14:10:40 -0300

    Primeira refatorada do exemplo 1, do capítulo 1

--- capitulo1-exemplo-pecas-teatro/versao-refatorada/main.js
+++ capitulo1-exemplo-pecas-teatro/versao-refatorada/main.js
@@ -1,6 +1,32 @@
 const plays = require('./plays.json');
 const invoices = require('./invoices.json');
 
+function amountFor(performance, play) {
+    let thisAmount = 0;
+
+    switch (play.type) {
+        case "tragedy":
+            thisAmount = 40000;
+            if (performance.audience > 30) {
+                // exemplo: performance.audience for 50, 1000 * 20 (performance.audience - 30), ou seja, vou somar no thisAmount 40000 + 20000
+                thisAmount += 1000 * (performance.audience - 30);
+            }
+            break;
+        case "comedy":
+            thisAmount = 30000;
+            if (performance.audience > 20) {
+                // exemplo: performance.audience for 50, 10000 + (500 * 30) (performance.audience - 20), ou seja, vou somar no thisAmount 30000 + 10000 + 15000
+                thisAmount += 10000 + 500 * (performance.audience - 20);
+            }
+            thisAmount += 300 * performance.audience;
+            break;
+        default:
+            throw new Error(`unknown type: ${play.type}`);
+    }
+
+    return thisAmount;
+}
+
 function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
@@ -8,27 +34,8 @@ function statement(invoice, plays) {
     const format = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD", minimumFractionDigits: 2}).format;
     for (let performance of invoice.performances) {
         const play = plays[performance.playID];
-        let thisAmount = 0;
-
-        switch (play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (performance.audience > 30) {
-                    // exemplo: performance.audience for 50, 1000 * 20 (performance.audience - 30), ou seja, vou somar no thisAmount 40000 + 20000
-                    thisAmount += 1000 * (performance.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (performance.audience > 20) {
-                    // exemplo: performance.audience for 50, 10000 + (500 * 30) (performance.audience - 20), ou seja, vou somar no thisAmount 30000 + 10000 + 15000
-                    thisAmount += 10000 + 500 * (performance.audience - 20);
-                }
-                thisAmount += 300 * performance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${play.type}`);
-        }
+        let thisAmount = amountFor(performance, play);
+        
         // soma créditos por volume
         volumeCredits += Math.max(performance.audience - 30, 0);
         if ("comedy" === play.type) volumeCredits += Math.floor(performance.audience / 5);
