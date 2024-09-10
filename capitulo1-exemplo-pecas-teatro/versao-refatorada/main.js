ecc27bb4935b41473107fc0b4cfaa2b79c7748e0
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2024-09-10 20:05:34 -0300

    Refatorando

--- capitulo1-exemplo-pecas-teatro/versao-refatorada/main.js
+++ capitulo1-exemplo-pecas-teatro/versao-refatorada/main.js
@@ -16,6 +16,7 @@ function renderPlainText(data) {
     return result;
 }
 
+
 function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {style: "currency", currency: "USD", minimumFractionDigits: 2}).format(aNumber / 100);
 }
